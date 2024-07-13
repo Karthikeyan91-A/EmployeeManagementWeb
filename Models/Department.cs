2d33b2324e3c35239fefb49af907234f3e80ac3b
karthikeyan <karthikeyan.91.cse@gmail.com>
2024-07-13 09:52:59 +0000

    Push changes

---
+++ Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace Employee_Management_Web.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+
+        public string DepartmentName { get; set; } = string.Empty;
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace Employee_Management_Web.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string? LastName { get; set; }
+
+        public int? DepartmentId { get; set; }
+        public virtual Department? Department { get; set; }
+    }
+}
